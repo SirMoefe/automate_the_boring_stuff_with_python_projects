f486bb5db577c86a0b7a86ee9c35dae1e8a0f8b6
SirMoefe <zeuch@pm.me>
2020-04-12 21:41:05 +0200

    Added empty list

--- Chapter_4-Lists/coin_flip_streaks.py
+++ Chapter_4-Lists/coin_flip_streaks.py
@@ -3,16 +3,17 @@
 import random
 
 numberOfStreaks = 0
-flip_list = []
+
 
 for experimentNumber in range(10000):
     for flip in range(100):
-
+        flip_list = []
         coin_side = random.randint(0, 1)
         if coin_side == 0:
             flip_list.append("H")
         else:
             flip_list.append("T")
+    print(flip_list)
 
 
-print("Chance of streake: %s%%" % (numberOfStreaks / 100))
+#print("Chance of streake: %s%%" % (numberOfStreaks / 100))
