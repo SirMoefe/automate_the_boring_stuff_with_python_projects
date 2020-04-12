45d1415d5f068c76703597c8c068491f0e13ac9c
SirMoefe <zeuch@pm.me>
2020-04-12 15:03:36 +0200

    Init commit

---
+++ Chapter_4-Lists/coin_flip_streaks.py
@@ -0,0 +1,18 @@
+"""Coin Flip Streaks"""
+
+import random
+
+numberOfStreaks = 0
+flip_list = []
+
+for experimentNumber in range(10000):
+    for flip in range(100):
+
+        coin_side = random.randint(0, 1)
+        if coin_side == 0:
+            flip_list.append("H")
+        else:
+            flip_list.append("T")
+
+
+print("Chance of streake: %s%%" % (numberOfStreaks / 100))
