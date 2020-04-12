fadc6cd8594fe3163db77683366c40d72aa4b3f2
SirMoefe <zeuch@pm.me>
2020-04-12 12:50:02 +0200

    Initial Commit

---
+++ Chapter_4-Lists/comma_code.py
@@ -0,0 +1,22 @@
+spam = ["apples", "bananas", "tofu", "cats"]
+ham = ["pig"]
+bacon = []
+
+
+def join_list(list):
+    if len(list) < 1:
+        return "The list is empty. Add some values!"
+    elif len(list) < 2 and len(list) > 0:
+        for value in list:
+            return value
+    else:
+        counter = 0
+        join = ""
+        while counter < len(list) - 1:
+            join += list[counter] + ", "
+            counter += 1
+        join += "and " + list[-1]
+        return join
+
+
+print(join_list(ham))
