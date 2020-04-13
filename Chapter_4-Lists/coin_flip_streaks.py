06dcb4258c9de6550b3a29b6f056a6575c237d52
SirMoefe <zeuch@pm.me>
2020-04-13 14:43:08 +0200

    changed the part to search for streaks

--- Chapter_4-Lists/coin_flip_streaks.py
+++ Chapter_4-Lists/coin_flip_streaks.py
@@ -15,27 +15,22 @@ for experimentNumber in range(10000):
             flip_list.append("H")
         else:
             flip_list.append("T")
-'''
-    temp_list = []
 
     for value in flip_list:
-        if len(temp_list) == 0:
-            temp_list.append(value)
-        elif value in temp_list and len(temp_list) < 6:
-            temp_list.append(value)
-        elif len(temp_list) == 6:
+        if current == None:
+            current = value
+            counter += 1
+        else:
+            checker = value
+            if current == checker:
+                current = checker
+                counter += 1
+            else:
+                current = checker
+                counter = 0
+        if counter == 6:
             numberOfStreaks += 1
-            del temp_list[:]
-'''
-for c in range(len(flip_list)):
-    current = checker
-    checker = flip_list[c]
-    if current == checker:
-        counter += 1
-    elif current != checker:
-        counter = 0
-    if counter == 6:
-        numberOfStreaks += 1
+            counter = 0
 
 
 print("Chance of streake: %s%%" % (numberOfStreaks / 100))
