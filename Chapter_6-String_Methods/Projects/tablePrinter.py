64c8e72a368a412ca683fe79951b72412afeaae8
moedi <zeuch@pm.me>
2020-06-26 23:04:53 +0200

    initial commit tablePrinter.py

---
+++ Chapter_6-String_Methods/Projects/tablePrinter.py
@@ -0,0 +1,11 @@
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+
+def printTable(table):
+    for value in table:
+        print(value)
+
+
+printTable(tableData)
